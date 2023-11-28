091bee80bef95edc8a720d6eb1c3f1eff9c94f99
juliagbmaciel <juliagabrielle863@gmail.com>
2023-11-28 11:29:17 -0300

    Setting token to localstorage

--- src/services/api/api.jsx
+++ src/services/api/api.jsx
@@ -7,6 +7,10 @@ export async function getToken(cpf, password) {
         cpf: cpf,
         password: password
       });
+      if (responseToken.data){
+        localStorage.setItem("token", responseToken.data.auth_token)
+      }  
+
       return responseToken.data;
     } catch (error) {
       console.log("erro ao fazer login;...")
@@ -25,9 +29,30 @@ export async function getAccount(token) {
           }
         }
       )
+
+
       return response.data
     } catch (error) {
       console.log("Deu erro")
       throw error
     }
   }
+
+
+  
+export async function getTransactions(token, type) {
+  try {
+    const response = await axiosInstance.get(`transfer/?type=${type}`,
+      {
+        headers: {
+          'Authorization': `Token ${token}`,
+        }
+      }
+    )
+    return response.data
+
+  } catch (error) {
+    console.log(error.response.data)
+    throw error
+  }
+}
